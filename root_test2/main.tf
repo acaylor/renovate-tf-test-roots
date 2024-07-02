1c929ee87d857eb244717f32143bfb4f8df0a1c7
acaylor <acaylor@redoxengine.com>
2024-07-02 12:05:16 -0600

    add test root 2

---
+++ root_test2/main.tf
@@ -0,0 +1,16 @@
+# Test module from terraform registry
+module "test_bucket2" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket = "test-bucket-name2"
+  acl = "private"
+
+  versioning = {
+    enabled = true
+  }
+}
+# Test module in git monorepo
+module "test_module2" {
+  source  = "github.com/acaylor/renovate-tf-test-modules.git//modules/test-module?ref=test-module2-v1.0.0"
+}
